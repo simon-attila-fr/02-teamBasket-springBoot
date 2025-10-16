44aeffef110e9335496429bd6e797d0e671486e1
Attila Simon <simon.attila.fr@gmail.com>
2025-10-16 15:00:45 +0200

    Joueur class + objects --> beginning

--- teamBasket/src/main/java/com/example/teambasket/dto/JoueurDTO.java
+++ teamBasket/src/main/java/com/example/teambasket/dto/JoueurDTO.java
@@ -1,9 +1,30 @@
 package com.example.teambasket.dto;
 
-public record JoueurDTO (
-        String nomJoueur,
-        String prenomJoueur,
-        String emailJoueur,
-        int nbEquipe
-) {
+public class JoueurDTO
+//        (
+//        String nomJoueur,
+//        String prenomJoueur,
+//        String emailJoueur,
+//        int nbEquipe
+//)
+        {
+    private String nomJoueur;
+    public String getNomJoueur() {
+        return nomJoueur;
+    }
+
+    private String prenomJoueur;
+    public String getPrenomJoueur() {
+        return prenomJoueur;
+    }
+
+    public String emailJoueur;
+    public String getEmailJoueur() {
+        return emailJoueur;
+    }
+
+    public int nbEquipe;
+    public int getNbEquipe() {
+        return nbEquipe;
+    }
 }
